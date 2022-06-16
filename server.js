41bc56f59b69382dddf8e916bffabb00214f85d1
Riya0405 <riya0405@chitkara.edu.in>
2022-06-17 00:00:09 +0530

    added express node and initial app created

---
+++ server.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const bodyParser= require ('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use('/',express.static('public'));
+
+app.get('/', function(req,res){
+    res.send("hello world");
+})
+
+app.listen(7762,function(){
+    console.log('server running on port 7762');
+})
